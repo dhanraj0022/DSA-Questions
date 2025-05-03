57a7dd31656296f182b91f307216124b6fe570fa
Dhanraj Singh <72537917+dhanraj0022@users.noreply.github.com>
2025-05-03 22:17:21 +0530

    Create nodes.java

---
+++ BinaryTree/nodes.java
@@ -0,0 +1,32 @@
+package Trees_GFG;
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val = val;
+    }
+}
+public class nodes {
+    public static void main(String[] args){
+        Node a = new Node(10); // Root
+        Node b = new Node(20); // a.left = b, a.right = c
+        Node c = new Node(30);
+        Node d = new Node(40); // b.left = d, b.right = e
+        Node e = new Node(50);
+        Node f = new Node(60); // c.left = f, c.right = g
+        Node g = new Node(70);
+        // connect
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.left = f; c.right = g;
+        print(a);
+    }
+    public static void print(Node root){
+        if(root == null) return;
+        System.out.println(root.val);
+        print(root.left);
+        print(root.right);
+    }
+}
